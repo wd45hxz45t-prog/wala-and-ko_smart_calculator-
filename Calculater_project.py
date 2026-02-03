f75befec62cd0535d6e9ccee847bdbd7e96d3f0d
walaa <wd45hxz45t@privaterelay.appleid.com>
2026-02-03 12:05:33 +0000

    Create Calculater_project.py
    initial project setup required imports

---
+++ Calculater_project.py
@@ -0,0 +1,13 @@
+# Import tkinter to build the graphical user interface (GUI):
+import tkinter as tk
+
+# Import math to perform mathematical operations such as roots and trigonometric functions:
+import math
+
+# Import re (Regular Expressions) to detect and transform mathematical patterns
+# such as square roots and scientific functions into valid Python expressions:
+import re
+
+
+# Stores the last calculated result so it can be reused using the "Ans" button:
+last_answer = ""
